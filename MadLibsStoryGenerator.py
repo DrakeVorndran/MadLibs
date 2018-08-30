7512f7a6b4dd57aabb1d36182f31c3b8851ea942
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-08-30 11:55:53 -0700

    generator works

--- MadLibsStoryGenerator.py
+++ MadLibsStoryGenerator.py
@@ -1,1 +1,18 @@
 inString = input("write a story you would like to be converted into madlibs")
+# inString = "write a story [name] would like to be converted into madlibs"
+index1 = inString.index(" ")
+
+outList = []
+
+while(index1<len(inString)-1):
+    if(inString[index1-1]=="]"):
+        outList.append([inString[1:index1-1],len(outList)])
+    else:
+        outList.append(inString[:index1])
+    inString = inString[index1+1:]
+    if(" " in inString):
+        index1 = inString.index(" ")
+    else:
+        index1 = len(inString)
+outList.append(inString[:index1])
+print (outList)
