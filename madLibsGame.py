98558dbea1c04b258730327791c66f4bb8a98539
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-08-30 10:30:39 -0700

    added the ReplaceWords function

--- madLibsGame.py
+++ madLibsGame.py
@@ -1,3 +1,4 @@
+from random import random
 storyList = ["hello",["name",1],"you","are","a","very",["adjective",6],"human"]
 
 def parseL(Story):
@@ -7,4 +8,11 @@ def parseL(Story):
             returnL.append(x[1])
     return returnL
 
-print(parseL(storyList))
+def Rwords(editible):
+    currEdit = 0
+    for x in range(len(editible)):
+        currEdit = editible.pop(int(random()*len(editible)))
+        storyList[currEdit] = input("please give me a {} word type: ".format(storyList[currEdit][0]))
+
+Rwords(parseL(storyList))
+print(storyList)
