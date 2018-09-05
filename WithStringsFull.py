233f00462ff654d73eb6388fe0ba43903cde9f7e
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-09-05 12:53:29 -0700

    added frank the fugitive

--- WithStringsFull.py
+++ WithStringsFull.py
@@ -3,8 +3,9 @@ from random import random
 storyD = {
 "The Pizza Place":['''Pizza was invented by a %s %s chef named  %s. To make a pizza, you need to take a lump of %s, and make a thin, round %s %s. Then you cover it with %s sauce, %s cheese, and fresh chopped %s. Next you have to bake it in a very hot %s. When it is done, cut it into %s %s. Some kids like %s pizza the best, but my favorite is the %s pizza. If I could, I would eat pizza %s times a day!.''' ,["adjective","nationality","person","noun","adjective","noun","adjective","adjective","plural noun","noun","number","shapes","food","food","number"]],
 
-"The Fugitive":['''A %s in %s was arrested this morning after he %s a %s in from of %s . %s , had a history of %s , but no one, not even his %s ever imagined he'd %s with a %s stuck in his %s . “I always thought he was %s , but I never thought he'd do something like this. Even his %s was surprised.” After a brief %s , cops followed him to a %s , where he reportedly %s in the fry machine. In %s, a woman was charged with a similar crime. But rather than %s with a %s , she %s with a %s . Either way, we imagine that after witnessing him %s with a %s there are probably a whole lot of %s''', ['noun', 'Wacky_State', 'verb', 'noun', 'noun', 'proper_name', 'verb', 'noun', 'verb','noun', 'part_of_the_body', 'adjective', 'Relative', 'activity', 'chain_restaurant', 'Past_tense_adjective', 'month', 'verb', 'noun', 'Past_tense_verb', 'Adjective', 'verb', 'noun', 'plural_noun']]
+"The Fugitive":['''A %s in %s was arrested this morning after he %s a %s in from of %s . %s , had a history of %s , but no one, not even his %s ever imagined he'd %s with a %s stuck in his %s . "I always thought he was %s , but I never thought he'd do something like this. Even his %s was surprised." After a brief %s , cops followed him to a %s , where he reportedly %s in the fry machine. In %s, a woman was charged with a similar crime. But rather than %s with a %s , she %s with a %s . Either way, we imagine that after witnessing him %s with a %s there are probably a whole lot of %s''', ['noun', 'Wacky_State', 'verb', 'noun', 'noun', 'proper_name', 'verb', 'noun', 'verb','noun', 'part_of_the_body', 'adjective', 'Relative', 'activity', 'chain_restaurant', 'Past_tense_adjective', 'month', 'verb', 'noun', 'Past_tense_verb', 'Adjective', 'verb', 'noun', 'plural_noun']],
 
+"Frank The Outlaw":['''"Put your hands up" said the %s. "You\'re under arrest for %s" the %s at the %s"."%s!" thought Frank. His hands were %s like %s on a %s. This wasn\'t the first time he had %s a %s and he knew he was in %s trouble. Just moments before Frank %s into a %s and %s a %s and a %s. He was spotted and a big %s chased after him and %s him in the %s. Now frank was surrounded and had no choice but to surrender. So it was off to %s for Frank. Frank was the %s sheep of his %s, always %s into trouble. It started when Frank %s a %s from a neighbor and %s rode the %s home''', ['noun', 'verb ending in ing', 'noun', 'noun', 'exclamation', 'verb ending in ing', 'plural noun', 'noun', 'past tense verb', 'noun', 'adjective', 'past tense verb', 'noun', 'past tense verb', 'noun', 'noun', 'noun', 'past tense verb', 'body part', 'place', 'color', 'noun', 'verb ending in ing', 'past tense verb', 'noun', 'adverb', 'noun']]
 
 }
 
@@ -50,9 +51,9 @@ def chooseStory():
 
 
 def test():
-    chosenStory = storyD["The Pizza Place"]
+    chosenStory = storyD["Frank The Outlaw"]
     for x in range(len(chosenStory[1])):
-        chosenStory[1][x] = (colored(getIn(chosenStory[1][x]+':',"cpu",x),"yellow",attrs=['bold']))
+        chosenStory[1][x] = (colored(getIn(chosenStory[1][x]+':',"cpu",chosenStory[1][x]),"yellow",attrs=['bold']))
     print (chosenStory[0] % tuple(chosenStory[1]))
 
 def getWords(cS):
